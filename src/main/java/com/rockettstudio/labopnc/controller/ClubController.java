ce055844aeee676043e590417926bc0e31cd6a06
DanielQuesada97 <00147619@uca.edu.sv>
2025-06-14 08:15:07 -0600

    add fork

---
+++ src/main/java/com/rockettstudio/labopnc/controller/ClubController.java
@@ -0,0 +1,34 @@
+package com.rockettstudio.labopnc.controller;
+import com.rockettstudio.labopnc.dto.ClubResponse;
+import com.rockettstudio.labopnc.dto.CreateClubRequest;
+import com.rockettstudio.labopnc.service.ClubService;
+import lombok.RequiredArgsConstructor;
+import jakarta.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/club")
+@RequiredArgsConstructor
+public class ClubController {
+    private final ClubService clubService;
+
+    @PostMapping
+    public ResponseEntity<ClubResponse> createProduct(@Valid @RequestBody CreateClubRequest request) {
+        return ResponseEntity.ok(clubService.createProduct(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClubResponse>> getAllProducts() {
+        return ResponseEntity.ok(clubService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClubResponse> getProductById(@PathVariable UUID id) {
+        return ResponseEntity.ok(clubService.getProductById(id));
+    }
+}
